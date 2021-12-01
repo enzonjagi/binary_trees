462cfb80537c1f2e29242dedb5b485600e669803
enzonjagi <amonsdungo@gmail.com>
2021-12-01 17:28:20 +0300

    Debugging(2-binary_tree_insert_right.c): Removed the free(new_node)

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,11 +10,10 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-	if (new_node == NULL)
-		return (free(new_node), NULL);
-	if (parent == NULL)
+	if (parent == NULL || new_node == NULL)
+	{
 		return (NULL);
-
+	}
 	new_node->n = value;
 	/* Check if the parent has a right child */
 	if (parent->right != NULL)
