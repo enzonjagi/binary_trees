c6e3125bddc66caf46728ae839575edafeaf15f2
miheret-eng <mihherettad2007@gmail.com>
2021-12-01 08:06:13 -0800

    inorder

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -7,7 +7,7 @@
  * Return: If the node is a root - 1.
  *         Otherwise - 0.
  */
-int binary_tree_is_root(const binary_tree_t *node) 
+int binary_tree_is_root(const binary_tree_t *node)
 {
 if (node == NULL || node->parent != NULL)
 return (0);
