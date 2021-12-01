4559ec4c2271ab4de589d058a84c8bdf5ec5d5d0
enzonjagi <amonsdungo@gmail.com>
2021-12-01 18:14:06 +0300

    Debugging(2-binary_tree_insert_right.c): new_node->n change

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -16,7 +16,6 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		return (NULL);
 	}
 
-	new_node->n = value;
 	/* Check if the parent has a right child */
 	if (parent->right != NULL)
 	{
@@ -24,6 +23,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		new_node->right = parent->right;
 		parent->right->parent = new_node;
 	}
+	new_node->n = value;
 	parent->right = new_node;
 	new_node->parent = parent;
 	new_node->left = NULL;
